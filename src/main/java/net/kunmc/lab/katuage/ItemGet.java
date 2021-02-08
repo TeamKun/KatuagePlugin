bbcb4672e7f579cb06d85fa605a5102f249c06bf
yanayana8721 <76870837+yanayana8721@users.noreply.github.com>
2021-02-08 21:14:55 +0900

    Add files via upload

---
+++ src/main/java/net/kunmc/lab/katuage/ItemGet.java
@@ -0,0 +1,22 @@
+package net.kunmc.lab.katuage;
+
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+
+public class ItemGet implements Listener {
+    //ドロップアイテム入手時の処理
+    @EventHandler
+    public void onPickUpItem(PlayerPickupItemEvent event){
+        Item i = event.getItem();
+        Player p = event.getPlayer();
+        if(i.getCustomName()==null){
+            event.setCancelled(false);
+        }
+        else if(i.getCustomName().equals("KatuagePlugin"+p.getName())){
+            event.setCancelled(true);
+        }
+    }
+}
